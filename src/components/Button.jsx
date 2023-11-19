5210776e48e33e95ced41142dd470b2685edd712
Mutiat Bashua <mutiatbashua@gmail.com>
2023-11-19 22:40:01 +0100

    added files

---
+++ src/components/Button.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+// import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+
+
+export const Button = ({ text, className, onClick }) => {
+    return (
+      <div>
+        <button
+        onClick={onClick}
+          className={`w-[fit-content] py-[0.8rem] px-2  whitespace-nowrap text-base md:text-lg bg-[#ff7518] font-bold rounded-md md:py-[0.6875rem] md:px-6 hover:bg-[#ff690d] hover:shadow-md transition-all ease-in ${className}`}
+        >
+          {text}
+        </button>
+      </div>
+    );
+  };
+ 
+  
+//   export const IconButton = ({ text, className, onClick, icon, iconSize, iconStyle }) => {
+//     return (
+//       <div>
+//         <button
+//         onClick={onClick}
+//           className={`w-[fit-content] flex flex-row justify-between items-center space-x-2 py-[0.8rem] px-2 whitespace-nowrap text-base md:text-lg bg-[#ff7518] font-bold rounded-md md:py-[0.6875rem] md:px-6 hover:bg-[#ff690d] hover:shadow-md transition-all ease-in ${className}`}
+//         >
+//             <span>{icon && <FontAwesomeIcon icon={icon} className="icon" style={iconStyle} size={iconSize}/>}</span>
+//          <span>{text}</span>
+//         </button>
+//       </div>
+//     );
+//   };
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
