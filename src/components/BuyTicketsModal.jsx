f3c0de4867782985dfa056f5934a613446fd6460
Mutiat Bashua <mutiatbashua@gmail.com>
2023-11-27 03:36:51 +0100

    added increse ticket func

---
+++ src/components/BuyTicketsModal.jsx
@@ -0,0 +1,194 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, IconButton } from "./Button";
+import {
+	faClose,
+	faCircleXmark,
+	faMinus,
+	faPlus,
+	faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import {
+	useContractReads,
+	useAccount,
+	useContractWrite,
+	usePrepareContractWrite,
+	useContractRead,
+} from "wagmi";
+import { token } from "../utils/testAbi";
+
+export const BuyTicketsModal = ({ events, onClose }) => {
+	const [count, setCount] = useState(0);
+	const [address, setAddress] = useState("");
+	const [amount, setAmount] = useState(0);
+
+	function handleIncreaseTickets() {
+		setAmount(amount + 1);
+	}
+
+    function handleDecreaseTickets() {
+        if(amount > 0){
+			setAmount(amount - 1);}
+		}
+	// WRITING TO THE CONTRACT
+
+	const { config, error } = usePrepareContractWrite({
+		address: token.address,
+		abi: token.abi,
+		functionName: "buyTickets",
+		args: [address, amount],
+	});
+
+	const {
+		data: buyData,
+		isLoading: writeLoading,
+		isSuccess: buySuccess,
+		write,
+	} = useContractWrite(config);
+
+	const handleSendToWallet = (e) => {
+		e.preventDefault();
+		write?.();
+	};
+	return (
+		<div className="bg-[#19181870] backdrop-blur w-full fixed mx-auto z-20 inset-x-0 inset-y-0 py-12 h-full overflow-y-hidden">
+			<div className="absolute cursor-pointer right-10 " onClick={onClose}>
+				<FontAwesomeIcon
+					icon={faCircleXmark}
+					style={{ color: "#fff", fontSize: "1.5rem" }}
+				/>
+			</div>
+			<div className="border border-[#D9D9D926] rounded-2xl bg-[#030203] mx-auto z-20 h-full py-8 pt-0 w-[70%] overflow-y-scroll relative pb-0">
+				<div className="absolute top-0 right-0">
+					<img src="/images/form-gradient-right.svg" alt="gradient" />
+				</div>
+				<div className="h3-head">
+					<h3 id="heading3">Buy Tickets</h3>
+				</div>
+				<div className="Purchase-form">
+					<div className="box">
+						<div className="flex flex-col gap-3 py-6">
+							<p className="p-ticket">
+								Purchase tickets that serve as NFTs that can be owned
+								and minted.
+								<p className="py-3 text-sm">
+									Enter your wallet address or email address
+								</p>
+							</p>
+							<div className="">
+								<form action="" className="forms py-2">
+									<label htmlFor="Wallet Address">
+										<span className="Wallet-Address">
+											Wallet Address
+										</span>
+										<input
+											value={address}
+											onChange={(e) => setAddress(e.target.value)}
+											type="text"
+											className="input-form placeholder:text-[#d9d9d941]"
+											placeholder="0x078h9uvribu9oupytvtytuyuuy"
+										/>
+									</label>
+
+									<label htmlFor="Email Address">
+										<span className="Wallet-Address">
+											Email Address
+										</span>
+										<input
+											type="email"
+											className="input-form placeholder:text-[#d9d9d941]"
+											placeholder="johndoe@gmail.com"
+										/>
+									</label>
+								</form>
+							</div>
+
+							<div className="flex flex-row gap-3 pt-3 justify-start items-center">
+								<p className="ticket-h3">Tickets</p>
+								<p className="ticket-p text-xs pt-0 font-bold">Free</p>
+							</div>
+							<div className="grid-form">
+								<div className="flex flex-row justify-between items-center p-4 py-5">
+									<p className="text-[#D9D9D9CC]">General admission</p>
+
+									<div className="grid-3 gap-5 items-center">
+										<button
+											className={`bg-[#6D676745] py-0.5 px-2 border border-[#6D6767] rounded-lg cursor-pointer ${
+												amount === 1 ? "bg-[#F57328]" : ""
+											}`}
+										>
+											<FontAwesomeIcon
+												icon={faMinus}
+												style={{ color: "#D9D9D9B2" }}
+											/>
+										</button>
+
+										<p className="text-[#F57328]">{amount}</p>
+
+										<button
+											className="bg-[#F57328] py-0.5 px-2 rounded-lg cursor-pointer"
+											onClick={handleIncreaseTickets}
+										>
+											<FontAwesomeIcon
+												icon={faPlus}
+												style={{ color: "#FFFDFC" }}
+												onClick={handleIncreaseTickets}
+											/>
+										</button>
+									</div>
+								</div>
+								<hr className="hr-line"></hr>
+								<div className="flex flex-row justify-between items-center p-4 py-4">
+									{/* <p className="text-[#D9D9D9CC]">Vip</p>
+
+									<div className="grid-3 gap-5 items-center">
+										<button
+											className={` bg-[#6D676745] py-0.5 px-2 border border-[#6D6767] rounded-lg`}
+										>
+											<FontAwesomeIcon
+												icon={faMinus}
+												style={{ color: "#D9D9D9B2" }}
+											/>
+										</button>
+
+										<p className="text-[#F57328]">{amount}</p>
+
+										<button className="bg-[#F57328] py-0.5 px-2 rounded-lg">
+											<FontAwesomeIcon
+												icon={faPlus}
+												style={{ color: "#FFFDFC" }}
+											/>
+										</button>
+									</div> */}
+								</div>
+							</div>
+
+							<div className="py-10">
+								<IconButton
+									text="Buy Tickets"
+									icon="/images/arrow-up-right.svg"
+									iconStyle={{ color: "#FFFDFC" }}
+									className="text-[#FFFDFC] font-['Stoke'] text-base "
+								/>
+							</div>
+						</div>
+
+						<div className="vertical"></div>
+						<div className="second">
+							<div>
+								<img src={events?.src} alt="" className="hack-img" />
+							</div>
+							<div className="flex flex-col gap-8 justify-center">
+								<p className="cart-h6">Add tickets to your cart</p>
+								<FontAwesomeIcon
+									icon={faShoppingCart}
+									style={{ color: "#6D6767" }}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/pages/Events/EventsDetails.jsx
+++ src/pages/Events/EventsDetails.jsx
@@ -12,15 +12,22 @@ import {
 import { useState, useContext } from "react";
 import { EventsCard } from "../../components/Cards";
 import { Button, IconButton } from "../../components/Button";
+import Footer from "../../components/Footer";
+import { BuyTicketsModal } from "../../components/BuyTicketsModal";
 
 const EventsDetails = () => {
 	const { eventId } = useParams();
 	//   const { addToCart, setCartItems } = useContext(CartContext);
-
+   const [buyModal, setBuyModal] = useState(false)
 	const event = events;
 	const selectedEvent = event?.paid?.find(
 		(event) => event.id === parseInt(eventId)
 	);
+
+    function openBuyModal(){
+        // set
+        setBuyModal(!buyModal)
+    }
 
 	//   function handleAddToCart() {
 	//     let itemAdded = false;
@@ -44,88 +51,92 @@ const EventsDetails = () => {
 	//   }
 
 	return (
-		<section className="p-4 py-4 md:p-10 lg:px-16 lg:py-10 flex flex-col my-auto h-full">
-			<Link to="/events">
-				<div className="py-4">
-					<FontAwesomeIcon
-						icon={faArrowLeftLong}
-						size="xl"
-						style={{ color: "#FFFDFC" }}
-					/>
-				</div>
-			</Link>
-			{selectedEvent ? (
-				<div className="flex flex-col sm:flex-row justify-between gap-12 md:gap-14 h-full py-6 lg:py-10 lg:pb-16">
-					<div
-						className="border border-[#D9D9D926] rounded-2xl bg-[#19181833] w-[45%] p-5"
-						key={events.id}
-					>
-						<div className="w-full">
-							<div className="flex flex-col gap-3 lg:gap-5">
-								<div className="relative">
-									<p className="absolute">
-										{selectedEvent?.tags.map((tag) => {
-											return <span>{tag.tags}</span>;
-										})}
-									</p>
-									<img
-										className="w-full h-35 rounded-lg"
-										src={selectedEvent.src}
-									/>
-								</div>
-								<h3 className="font-['Stoke'] text-[#FFFDFC] text-2xl leading-[2.8rem]">
-									{selectedEvent.title}
-								</h3>
-								<div className="flex flex-col gap-1.5 pb-1 justify-self-end">
-									<div className="flex flex-row gap-2 items-center">
-										<p className="text-zinc-300 text-opacity-80 font-normal font-['Manrope']">
-											{selectedEvent.date}
+		<div className="relative">
+			<section className="p-4 py-4 md:p-10 lg:px-16 lg:py-10 flex flex-col my-auto h-full">
+				<Link to="/events">
+					<div className="py-4">
+						<FontAwesomeIcon
+							icon={faArrowLeftLong}
+							size="xl"
+							style={{ color: "#FFFDFC" }}
+						/>
+					</div>
+				</Link>
+				{selectedEvent ? (
+					<div className="flex flex-col sm:flex-row justify-between gap-12 md:gap-14 h-full py-6 lg:py-10 lg:pb-16">
+						<div
+							className="border border-[#D9D9D926] rounded-2xl bg-[#19181833] w-[45%] p-5"
+							key={events.id}
+						>
+							<div className="w-full">
+								<div className="flex flex-col gap-3 lg:gap-5">
+									<div className="relative">
+										<p className="absolute">
+											{selectedEvent?.tags.map((tag) => {
+												return <span>{tag.tags}</span>;
+											})}
 										</p>
-										<span className="text-zinc-300 text-opacity-80">
-											.
-										</span>
-										<p className="text-zinc-300 text-opacity-80 font-normal font-['Manrope']">
-											{selectedEvent.time}
+										<img
+											className="w-full h-35 rounded-lg"
+											src={selectedEvent.src}
+										/>
+									</div>
+									<h3 className="font-['Stoke'] text-[#FFFDFC] text-2xl leading-[2.8rem]">
+										{selectedEvent.title}
+									</h3>
+									<div className="flex flex-col gap-1.5 pb-1 justify-self-end">
+										<div className="flex flex-row gap-2 items-center">
+											<p className="text-zinc-300 text-opacity-80 font-normal font-['Manrope']">
+												{selectedEvent.date}
+											</p>
+											<span className="text-zinc-300 text-opacity-80">
+												.
+											</span>
+											<p className="text-zinc-300 text-opacity-80 font-normal font-['Manrope']">
+												{selectedEvent.time}
+											</p>
+										</div>
+
+										<p className="font-extralight text-[#fffdfcc9] font-['Stoke'] ">
+											{selectedEvent.venue}
+										</p>
+										<p className=" font-['Stoke'] text-[#F57328] ">
+											{selectedEvent.type}
 										</p>
 									</div>
-
-									<p className="font-extralight text-[#fffdfcc9] font-['Stoke'] ">
-										{selectedEvent.venue}
-									</p>
-									<p className=" font-['Stoke'] text-[#F57328] ">
-										{selectedEvent.type}
-									</p>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div className="flex flex-col justify-between gap-5 lg:gap-6 w-full md:w-[45%] ">
-						<div className="flex flex-col lg:gap-10 h-full justify-between w-full">
-							<h3 className="font-['Stoke'] text-[#FFFDFC] text-3xl leading-[2.8rem]">
-								{selectedEvent.title}
-							</h3>
-							<p className="text-base lg:text-lg text-[#D9D9D9D1] font-['Manrope'] tracking-wide text-justify">
-								{/* {selectedEvent.details} */}
-								HacktoberFest is a one-day event to be organized by
-								BusinessDay Media. This conference will serve as a
-								platform for all stakeholders in the creative industry,
-								including professionals in tech, film, fashion, gaming,
-								and animation, to engage in insightful discourse,
-								network, and explore investment opportunities. The
-								conference aims to highlight the challenges, potential,
-								and vast opportunities available in open source.
-							</p>
-							<IconButton
-								text="Buy Tickets"
-								icon="/images/arrow-up-right.svg"
-								iconStyle={{ color: "#FFFDFC" }}
-								className="text-[#FFFDFC] font-['Stoke'] self-end mt-auto"
-							/>
-						</div>
-						{/* <div className="flex flex-col gap-3">
-              <p>Every order comes with a free drink, select your choice</p>
-              </div>
+						<div className="flex flex-col justify-between gap-5 lg:gap-6 w-full md:w-[45%] ">
+							<div className="flex flex-col lg:gap-10 h-full justify-between w-full">
+								<h3 className="font-['Stoke'] text-[#FFFDFC] text-3xl leading-[2.8rem]">
+									{selectedEvent.title}
+								</h3>
+								<p className="text-base lg:text-lg text-[#D9D9D9D1] font-['Manrope'] tracking-wide text-justify">
+									{/* {selectedEvent.details} */}
+									HacktoberFest is a one-day event to be organized by
+									BusinessDay Media. This conference will serve as a
+									platform for all stakeholders in the creative
+									industry, including professionals in tech, film,
+									fashion, gaming, and animation, to engage in
+									insightful discourse, network, and explore investment
+									opportunities. The conference aims to highlight the
+									challenges, potential, and vast opportunities
+									available in open source.
+								</p>
+								<IconButton
+									text="Buy Tickets"
+									icon="/images/arrow-up-right.svg"
+									iconStyle={{ color: "#FFFDFC" }}
+									onClick={openBuyModal}
+									className="text-[#FFFDFC] font-['Stoke'] self-end mt-auto"
+								/>
+								{/* <div className=""> */}
+									{buyModal && <BuyTicketsModal events={selectedEvent} onClose={()=>setBuyModal(false)}/>}
+								{/* </div> */}
+							</div>
+							{/* 
             </div>
             <div className="flex flex-row justify-between lg:justify-start items-center gap-6 w-full">
               <div className="flex flex-row items-center">
@@ -156,20 +167,17 @@ const EventsDetails = () => {
                 className="text-white w-full px-6 "
               />
             </div> */}
+						</div>
 					</div>
-				</div>
-			) : (
-				<p> Event not found </p>
-			)}
-
-            {/* SIMILAR EVENTS */}
-			<div className="flex flex-col items-center justify-center py-8 lg:py-20 gap-8 relative lg:pt-24">
+				) : (
+					<p> Event not found </p>
+				)}
+			</section>
+			{/* SIMILAR EVENTS */}
+			<div className="flex flex-col items-center justify-center py-8 px-4 md:p-10 lg:px-16 lg:py-20 gap-8 relative lg:pt-24">
 				<h2 className="font-['Stoke'] text-[#FFFDFC] text-center text-lg lg:text-4xl">
 					Similar Events
 				</h2>
-				<div className="absolute top-0">
-					<img src="/images/center-gradient.svg" alt="gradient" />
-				</div>
 				<div className="grid lg:grid-cols-4 gap-8 gap-y-12 bg-[#020302] py-8 lg:py-14">
 					{events?.paid?.slice(0, 8).map((events, index) => {
 						return <EventsCard key={index} events={events} />;
@@ -180,7 +188,11 @@ const EventsDetails = () => {
 					className="text-[#F57328] border hover:cursor-pointer border-[#F57328] bg-transparent px-6 font-['Stoke']"
 				/>
 			</div>
-		</section>
+			<div className="absolute bottom-0 left-0">
+				<img src="/images/left-full-gradient.svg" alt="gradient" />
+			</div>
+			<Footer />
+		</div>
 	);
 };
 
