687a97f6cb134681fdafb37a18ba3875ad085654
Mutiat Bashua <mutiatbashua@gmail.com>
2023-11-28 23:49:59 +0100

    added create event form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState , useContext, useEffect} from 'react'
-import { Routes, Route,useNavigate} from "react-router-dom";
-import './App.css'
-import Navbar from './components/Navbar';
-import Login from './allPages/Signup'
-import Homepage from './allPages/Homepage'
-import EventsPage from './allPages/Events/Events'
-import EventsDetails from './allPages/Events/EventsDetails';
-import SignUp from './allPages/Signup';
-import { UserContext } from './Context';
-import { magic } from '../api/magic';
-
+import { useState, useContext, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./allPages/Signup";
+import Homepage from "./allPages/Homepage";
+import EventsPage from "./allPages/Events/Events";
+import EventsDetails from "./allPages/Events/EventsDetails";
+import CreateEvents from "./allPages/Events/CreateEvents";
+import SignUp from "./allPages/Signup";
+import { UserContext } from "./Context";
+import { magic } from "../api/magic";
 
 function App() {
+	const navigate = useNavigate();
+	const { user, setUser } = useContext(UserContext);
 
-  const navigate = useNavigate();
-  const { user, setUser } = useContext(UserContext);
-
-  return (
-    <>
-    {/* <Navbar/> */}
-    <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route
-            path="/event-details/:eventId"
-            element={<EventsDetails />}
-          />
-        </Routes>
-    </>
-  )
+	return (
+		<>
+			{/* <Navbar/> */}
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/events" element={<EventsPage />} />
+				<Route path="/create-events" element={<CreateEvents />} />
+				<Route path="/event-details/:eventId" element={<EventsDetails />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/sections/Home/HomeEvents.jsx
+++ src/sections/Home/HomeEvents.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import { EventsCard } from '../../components/Cards'
 import { events } from '../../data'
 import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 const HomeEvents = () => {
   return (
-    <div className='flex flex-col items-center justify-center px-2 lg:px-16 py-8 lg:py-14  gap-4 lg:gap-8 relative'>
-      <h2 className="font-['Stoke'] text-[#FFFDFC] text-center text-lg lg:text-4xl">Popular Events</h2>
-       <div className='absolute top-12 w-full lg:w-[50%] lg:top-5 pointer-events-none'><img src='/images/center-gradient.svg' alt="gradient"/></div>
-    <div className='grid lg:grid-cols-4 gap-8 bg-[#020302] py-8 lg:py-16'>
-      {
-        events?.paid?.slice(0,4).map((events, index) =>{
-          return(
-           <EventsCard key={index} events={events}/>
-          )
-        })
-      } 
-    </div>
-    <Button text="View more" className="text-[#F57328] border hover:cursor-pointer border-[#F57328] bg-transparent px-6 font-['Stoke'] self-center" />
-    </div>
-  )
+		<div className="flex flex-col items-center justify-center px-2 lg:px-16 py-8 lg:py-14  gap-4 lg:gap-8 relative">
+			<h2 className="font-['Stoke'] text-[#FFFDFC] text-center text-lg lg:text-4xl">
+				Popular Events
+			</h2>
+			<div className="absolute top-12 w-full lg:w-[50%] lg:top-5 pointer-events-none">
+				<img src="/images/center-gradient.svg" alt="gradient" />
+			</div>
+			<div className="grid lg:grid-cols-4 gap-8 bg-[#020302] py-8 lg:py-16" id="explore-events">
+				{events?.paid?.slice(0, 4).map((events, index) => {
+					return <EventsCard key={index} events={events} />;
+				})}
+			</div>
+			<Link to="/events">
+				<Button
+					text="View more"
+					className="text-[#F57328] border hover:cursor-pointer border-[#F57328] bg-transparent px-6 font-['Stoke'] self-center"
+				/>
+			</Link>
+		</div>
+  );
 }
 
 export default HomeEvents
